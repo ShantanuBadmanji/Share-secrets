0c10583fe5e4ed9aa32527861cc9ad8c8d972aa8
Shantanu Badmanji <shantanubadmanji1912@gmail.com>
2023-06-27 20:06:25 +0530

    level 5: Manualauthentication. Establishing session using Cokkies.

--- app.js
+++ app.js
@@ -5,34 +5,46 @@ const express = require('express');
 const ejs = require('ejs');
 const exp = require('constants');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltCount = 10;
-
-mongoose
-    .connect("mongodb://127.0.0.1:27017/userDB")
-    .then((res) => console.log(`SuccessFully connected ${res}`))
-    .catch(err => console.log(err.message))
-
-
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-});
-
-const User = mongoose.model('User', userSchema);
-
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+//configure a session.
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose
+    .connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true })
+    .then(() => console.log(`db connected.`))
+    .catch(err => console.log(err.message))
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -43,43 +55,50 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.render('register');
 })
+app.get('/secrets', (req, res) => {
+    if (!req.isAuthenticated()) {
+        res.redirect("/login");
+    }
+    res.render('secrets');
+})
+app.get('/submit', (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.redirect("/login");
+    }
+    res.render('submit');
+})
+app.get('/logout', (req, res) => {
+    req.logout(err => {
+        if (err)
+            return res.send(`logout ERROR: ${err.message}`)
+    });
+    res.redirect('/');
+})
+
+
 
 
 app.post('/register', (req, res) => {
-    // console.log(req.body)
-    bcrypt
-        .hash(req.body.password, saltCount)
-        .then((hash) => {
-            new User({
-                email: req.body.username,
-                password: hash
+    User.register(
+        new User({ username: req.body.username }), req.body.password)
+        .then(user => {
+            req.login(user, (err) => {
+                if (err) {
+                    return res.send(err.message);
+                }
             })
-                .save()
-                .then(() => res.redirect('login'))
-                .catch(err => res.send(err.message));
+            res.redirect('/secrets');
         })
-        .catch(err => res.send(err.message));
+        .catch((err) => res.send(`registration error: ${err.message}`))
+})
+app.post('/login', passport.authenticate(
+    'local',
+    { successRedirect: '/secrets', failureRedirect: '/login' })
+);
+app.post("/submit", (req, res) => {
+    res.send(`yet to bet set`)
 })
 
-app.post('/login', (req, res) => {
-    // console.log(req.body);
-    (async () => {
-        try {
-            const doc = await User.findOne({ email: req.body.username });
-            if (!doc)
-                res.send('Invalid username.')
-            else {
-                if (await bcrypt.compare(req.body.password, doc.password))
-                    res.render('secrets');
-                else
-                    res.send('wrong password.');
-            }
-        } catch (err) {
-            res.send(err.message);
-        }
-    })();
-})
-
-
+const port = 3000
 app.listen(3000,
-    (arg) => console.log(`Successfully listening on port 3000 : ${arg}`))
+    () => console.log(`Server listening on port ${port}.`));
